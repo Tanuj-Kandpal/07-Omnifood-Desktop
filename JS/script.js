3918c2ba0e25e4a0e383a9e7beb894cc53e33fe6
Tanuj-Kandpal <108190243+Tanuj-Kandpal@users.noreply.github.com>
2022-12-15 20:51:52 +0530

    1. Added javascript for smooth scrolling 2. Added query css for responsive design

---
+++ JS/script.js
@@ -0,0 +1,87 @@
+// Current year selection
+const year = document.querySelector(".current-year");
+const currentyear = new Date().getFullYear();
+console.log(currentyear);
+year.textContent = currentyear;
+
+// Toggle nav bar
+
+const header = document.querySelector(".header");
+
+const buttonNav = document.querySelector(".btn-mobile-nav");
+
+buttonNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+// for smooth scroling apart from safari browser
+
+const alllinks = document.querySelectorAll("a");
+
+alllinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      header.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
